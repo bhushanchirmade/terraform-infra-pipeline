3a6f7a4594bfd763174a944c0116caed608efaf9
bhushanchirmade <bhushanchirmade@gmail.com>
2020-07-16 18:27:56 -0500

    Removed duplicate module.

--- main.tf
+++ main.tf
@@ -13,13 +13,6 @@ module "codecommit" {
   aws_region    = var.aws_region
   default_tags  = var.default_tags
 }
-  
-module "codecommit" {
-  source        = "./modules/codecommit"
-  environment   = var.environment
-  aws_region    = var.aws_region
-  default_tags  = var.default_tags
-}
 
 
 
